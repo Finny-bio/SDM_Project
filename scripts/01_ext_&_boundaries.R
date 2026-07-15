563b6d5a74a35978fcfbbf9b6d73dcc2e96ab1e5
Finny-bio <finnwalsh20006@gmail.com>
2026-07-15 14:19:33 +0100

    Fix environmental predictor masking (Isle of Man, coastal gaps); add predictor collinearity check

--- scripts/01_ext_&_boundaries.R
+++ scripts/01_ext_&_boundaries.R
@@ -12,3 +12,11 @@ ie_wkt <- st_as_text(st_as_sfc(ie_bbox))
 
 gb_boundary <- gadm(country = "GBR", level = 0, path = "data/")
 ie_boundary <- gadm(country = "IRL", level = 0, path = "data/")
+
+imn_boundary <- gadm(country= "IMN",level = 0, path = "data/")
+gb_full <- rbind(gb_boundary, imn_boundary)
+
+gb_mask <- mask(gb_crop, gb_full)
+bioclim_study <- merge(gb_mask, ie_masked)
+
+writeRaster(bioclim_study, "data/bioclim_study.tif", overwrite = TRUE)
